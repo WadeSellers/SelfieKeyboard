637212a2458335583347dca5cde74bc3bab14d93
Wade Sellers <Waderoni@gmail.com>
2014-10-26 21:41:10 -0500

    Initial Commit

---
+++ SelfieKeyboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SelfieKeyboard
+//
+//  Created by Wade Sellers on 10/26/14.
+//  Copyright (c) 2014 Wade Sellers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SelfieKeyboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SelfieKeyboard
+//
+//  Created by Wade Sellers on 10/26/14.
+//  Copyright (c) 2014 Wade Sellers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
